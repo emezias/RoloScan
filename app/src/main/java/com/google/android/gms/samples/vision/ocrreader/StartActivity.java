a04fc622860e10fd7dd89cfcaec0915e131ec8ec
emezias <emezias@gmail.com>
2017-04-30 11:57:34 -0400

    trying out a new icon and app name - meet RoboScan

--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/StartActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/StartActivity.java
@@ -48,6 +48,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        getSupportActionBar().setTitle(R.string.label);
     }
 
     public void getPhoto(View v) {

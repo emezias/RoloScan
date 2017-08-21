6d6b334b4f777a10726038d69155a02f49723104
emezias <emezias@gmail.com>
2017-08-20 22:10:30 -0400

    Bit more polish, no more mipmap to try and make the Samsung icon look better

--- app/src/main/java/com/mezcode/demo/roloscan/ocrreader/StartActivity.java
+++ app/src/main/java/com/mezcode/demo/roloscan/ocrreader/StartActivity.java
@@ -154,9 +154,6 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_start);
     }
 
